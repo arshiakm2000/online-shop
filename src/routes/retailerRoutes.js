3cd3552c8ff3a2756f367fde0425d6911901e2b2
arshiaMotamedi <akeshavarzmotamedi@myseneca.ca>
2024-11-10 11:41:50 -0600

    finished photo upload functions

--- src/routes/retailerRoutes.js
+++ src/routes/retailerRoutes.js
@@ -5,4 +5,5 @@ const router = Router();
 router.get("/addItem", retailerController.addItem_get);
 router.post("/addItem", retailerController.addItem_post);
 router.get("/uploadPhoto", retailerController.photoUpload_get);
+router.post("/uploadPhoto", retailerController.photoUpload_post);
 module.exports = router;
